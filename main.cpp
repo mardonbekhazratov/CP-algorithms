9251bb9e25ccaf68d1dd10936587c404f032909e
mardonbekh <97873195+mardonbekh@users.noreply.github.com>
2023-04-17 18:54:26 +0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+//#include<bits/stdc++.h>
+#include<iostream>
+#include<set>
+#include<algorithm>
+#include<vector>
+#define ll long long
+
+using namespace std;
+
+void solve(){
+    
+}
+
+int main(){
+    int t=1;
+    //cin>>t;
+    while(t--){
+        solve();
+    }
+}
