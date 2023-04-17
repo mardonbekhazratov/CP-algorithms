448bc28b52836a128b433db191f8a69ee663e8d0
mardonbekh <97873195+mardonbekh@users.noreply.github.com>
2023-04-17 18:55:44 +0500

    Create fast.cpp

---
+++ fast.cpp
@@ -0,0 +1,20 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#pragma GCC target("avx,avx2")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+
+}
+
+int main() {
+    ios_base::sync_with_stdio( false ); cin.tie( nullptr ); cout.tie( nullptr );
+    int q = 1;
+    cin >> q;
+    while (q--) {
+        solve();
+        cout << '\n';
+    }
+}
